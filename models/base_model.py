e83fc46b99794e0bc90ada243289bacd507b1434
matxa <matxa21@gmail.com>
2020-06-29 13:33:18 -0400

    end of file engine implementation

---
+++ models/base_model.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+""" BaseModel class which all other classes inhirit from """
+from datetime import datetime
+import models
+import uuid
+
+
+class BaseModel():
+    """Base Model"""
+    def __init__(self, *args, **kwargs):
+        """Init
+        """
+        if kwargs:
+            for k, v in kwargs.items():
+                if k == "created_at":
+                    kwargs[k] = datetime.strptime(v, "%Y-%m-%dT%H:%M:%S.%f")
+                if k == "updated_at":
+                    kwargs[k] = datetime.strptime(v, "%Y-%m-%dT%H:%M:%S.%f")
+                if k != "__class__":
+                    setattr(self, k, kwargs[k])
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            models.storage.new(self)
+
+    def __str__(self):
+        """string
+        repr
+        """
+        s = ("[{}] ({}) {}".format(BaseModel.__name__, self.id, self.__dict__))
+        return s
+
+    def save(self):
+        """save the updated
+        time of last modification
+        """
+        self.updated_at = datetime.now()
+        models.storage.save()
+
+    def to_dict(self):
+        """dictionary containing
+        all keys
+        """
+        self.__dict__["created_at"] = self.__dict__["created_at"].isoformat()
+        self.__dict__["updated_at"] = self.__dict__["updated_at"].isoformat()
+        self.__dict__.update({"__class__": BaseModel.__name__})
+        return self.__dict__
---
+++ models/engine/file_storage.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+""" FileStorage class which serializes/deserializes JSON file to instances """
+import json
+import os.path
+from os import path
+from models.base_model import BaseModel
+
+class FileStorage():
+    """ FileStorage """
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """all
+        key, v
+        """
+        return FileStorage.__objects
+
+    def new(self, obj):
+        """new
+        obj
+        """
+        key = type(obj).__name__ + "." + obj.id
+        FileStorage.__objects[key] = obj
+
+    def save(self):
+        """save to file
+        """
+        with open(FileStorage.__file_path, 'w') as file:
+            new_dict = {}
+            for k, v in FileStorage.__objects.items():
+                new_dict.update({k: v.to_dict()})
+            json.dump(new_dict, file)
+
+    def reload(self):
+        """getting from
+        file
+        """
+        if path.exists(FileStorage.__file_path):
+            with open(FileStorage.__file_path, 'r', encoding='utf-8') as file:
+                file_content = json.loads(file.read())
+                for k, v in file_content.items():
+                    obj_to_reload = eval(v['__class__'])(**v)
+                    FileStorage.__objects.update({k: obj_to_reload})
