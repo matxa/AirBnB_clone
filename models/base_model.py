2b4b19dd1a733e653863825f9f39c8b7da0ad67b
matxa <matxa21@gmail.com>
2020-06-30 13:16:55 -0400

    change BaseModel name to any class name

--- models/base_model.py
+++ models/base_model.py
@@ -13,16 +13,16 @@ class BaseModel():
         """
         if kwargs:
             for k, v in kwargs.items():
-                if k == "created_at":
+                if k == '__class__':
+                    continue
+                if k == 'created_at' or k == 'updated_at':
                     kwargs[k] = datetime.strptime(v, "%Y-%m-%dT%H:%M:%S.%f")
-                if k == "updated_at":
-                    kwargs[k] = datetime.strptime(v, "%Y-%m-%dT%H:%M:%S.%f")
-                if k != "__class__":
-                    setattr(self, k, kwargs[k])
+                setattr(self, k, kwargs[k])
         else:
+            time_now = datetime.now()
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+            self.created_at = time_now
+            self.updated_at = time_now
             models.storage.new(self)
 
     def __str__(self):
