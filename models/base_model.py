b30e7d330aafdd2cf3c1a67952f0559c8159e3cb
matxa <matxa21@gmail.com>
2020-06-30 13:33:05 -0400

    got rid of continue key word

--- models/base_model.py
+++ models/base_model.py
@@ -12,14 +12,11 @@ class BaseModel():
         """Init"""
         if kwargs:
             for k, v in kwargs.items():
-                if k == '__class__':
-                    continue
                 if k == 'created_at' or k == 'updated_at':
                     kwargs[k] = datetime.strptime(v, "%Y-%m-%dT%H:%M:%S.%f")
                     setattr(self, k, kwargs[k])
-                    continue
-
-                setattr(self, k, kwargs[k])
+                if k != '__class__':
+                    setattr(self, k, kwargs[k])
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
